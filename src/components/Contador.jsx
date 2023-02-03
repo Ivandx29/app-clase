b5d4f2467ebd157a6e9e81a499adbb07898da233
brisa <brisa12631@gmail.com>
2023-02-03 11:44:03 -0600

    diseno

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -38,32 +38,54 @@ const Contador = () => {
             width: 100,
             height: 100,
         },
+        card: {
+            margin: 2,
+            justifyContent: 'center',
+        },
+        container: {
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+          },
+          button: {
+            margin: 2,
+          },
+          controlContainer: {
+            borderRadius: 4,
+            margin: 2,
+            padding: 6,
+            justifyContent: 'center',
+            backgroundColor: '#3366FF',
+          },
     });
 
     return (
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Layout >
-                <Text>Esto es un Contador</Text>
-                <Text>{counter}</Text>
-                <Button onPress={incremento}> incremento </Button>
-                <Button onPress={decremento}> Decremento </Button>
-
-                <Button onPress={() => { setIsVisible(true); obtenerPersona(); }}> Ver Persona </Button>
+                <Text category='h6'>Esto es un Contador</Text>
+                <Text category='h5' >{counter}</Text>
+                <Layout style={styles.container}>
+                <Button style={{margin:2}} status='warning' onPress={incremento}> incremento </Button>
+                <Button style={{margin:2}} status='danger' onPress={decremento}> Decremento </Button>
+                
+                </Layout>
+                <Button appearance='ghost' status='primary' onPress={() => { setIsVisible(true); obtenerPersona(); }}> Ver Persona </Button>
                 <Modal visible={isVisible}>
-                    <Card >
-                        <Image
-                            style={styles.logo}
-                            source={{ uri: `${persona?.picture?.medium}` }}
-                        />
-                        <Text>Nombre de Sujeto : {persona?.name?.first}</Text>
-                        <Text>Edad del Sujeto : {persona?.dob?.age}</Text>
-                        <Text>Email del Sujeto : {persona?.email}</Text>
-                        <Button onPress={() => setIsVisible(false)}>
-                            Cerrar
-                        </Button>
+                    <Card style={styles.card} status='success'>
+                        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+                            <Image
+                                style={styles.logo}
+                                source={{ uri: `${persona?.picture?.medium}` }}/>
+                            <Text  style={{marginBottom:2}} category='h6'>Nombre de Sujeto : {persona?.name?.first}</Text>
+                            
+                            <Text  style={{marginBottom:2}} >Edad del Sujeto : {persona?.dob?.age}</Text>
+                            <Text  style={{marginBottom:2}} >Email del Sujeto : {persona?.email}</Text>
+                            <Button   style={{marginTop:2}}appearance='outline' status='success' onPress={() => setIsVisible(false)}>
+                                Cerrar
+                            </Button>
+                        </Layout>
                     </Card>
                 </Modal>
-            </Layout>
+            
         </Layout>
     )
 };
